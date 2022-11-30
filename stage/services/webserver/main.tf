f41f77a00b0a91a8f3267672458917af56358d69
jharter1 <jharter1@users.noreply.github.com>
2022-11-30 12:33:36 -0600

    Update main.tf

--- stage/services/webserver/main.tf
+++ stage/services/webserver/main.tf
@@ -1,12 +1,60 @@
+terraform {
+  required_version = ">=1.0.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+
 provider "aws" {
   region = "us-east-2"
 }
 
 resource "aws_instance" "webserver_1" {
-  ami = "ami-0d5bf08bc8017c83b"
-  instance_type = "t2.micro"
+  ami                    = "ami-0d5bf08bc8017c83b"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance_grouper.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello there, big guy" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
+
+  user_data_replace_on_change = true
 
   tags = {
-    "Name" = "lil_servy"
+    "Name" = "minor_server"
   }
+}
+
+resource "aws_security_group" "instance_grouper" {
+  name = var.security_group_name
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+variable "security_group_name" {
+  description = "Group name for instance(s)"
+  type        = string
+  default     = "basic_group"
+}
+
+output "end_location" {
+
+  value       = aws_instance.webserver_1.public_ip
+  description = "public IP of said instance"
 }
