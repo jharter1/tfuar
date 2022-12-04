59fa34a1468b695cf5ad6de9bba74fad2ac2e582
jharter1 <jharter1@users.noreply.github.com>
2022-12-04 13:56:07 -0600

    modularization

---
+++ prod/services/webserver-cluster/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-2"
+}
+
+module "webserver_cluster" {
+  source = "../../../modules/services/webserver-cluster"
+  cluster_name = "webservers-prod"
+  db_remote_state_bucket = "state-bucket-122022"
+  db_remote_state_key = "prod/data-stores/mysql/terraform.tfstate"
+}
